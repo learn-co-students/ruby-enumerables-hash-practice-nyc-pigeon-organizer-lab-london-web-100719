30ca216e509eb3d8d02a61ce721eff3e461a27bd
Li Xie <li_xie1992@hotmail.com>
2019-09-12 20:29:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,33 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  name_array = []
+  
+  (data[:gender][:male] + data[:gender][:female]).each do |name|
+    name_array.push(name)
+  end
+  
+  name_array.collect do |name|
+    new_hash[name] = {
+      [:color] => [],
+      [:gender] => [],
+      [:lives] => []
+    }
+  end
+  
+  data.each do |key, value|
+    value.each do |key2, value2|
+      if (key == :color || key == :gender)
+        key2 = key2.to_s #only 'lives' will correctly be added to hash without converting color and gender
+      end 
+      value2.each do |item|
+        if (new_hash[item][key] == nil) # cover case where pigeon has more than 1 color
+          puts new_hash[item][key] = [key2]
+        else
+          puts new_hash[item][key] += [key2]
+        end
+      end
+    end
+  end
+  
+  return new_hash
 end
