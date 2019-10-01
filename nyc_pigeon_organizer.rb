51db518cd168cd82514f18a026045493e77d55eb
Caroline S-A <caroline.seilern@hotmail.com>
2019-10-01 09:12:30 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,64 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+   
+  new_hash = {}
+  
+  data.each do |key,value| 
+    value.each do |descriptor, names|
+      names.each do |pigeon|
+        
+        if !new_hash[pigeon]
+          new_hash[pigeon] = {} 
+        end
+        
+        if !new_hash[pigeon][key]
+          new_hash[pigeon][key] =[]
+        end
+        
+          new_hash[pigeon][key] << descriptor.to_s
+        
+        
+      end
+    end
+  end
+  
+  new_hash
+  
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def nyc_pigeon_organizer1(data)
+  
+  new_hash = {}
+
+data.each do |attributes|
+  if data[attributes]
+  data[attributes].each do |descriptor|
+    data[attributes][descriptor].each do |pidgeon|
+      if !new_hash[pidgeon]
+      new_hash[pidgeon] = {}
+      end
+    end
+  end
+    
+  end
+  end
+data
 end
