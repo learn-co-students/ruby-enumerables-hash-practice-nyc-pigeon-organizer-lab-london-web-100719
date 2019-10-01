f1b08e501a40c368841a6b0a89c71e75f42ec96e
Faris Aziz <farisaziz12@gmail.com>
2019-10-01 12:43:13 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  p_hash = {}
+
+ data[:color].each do |color, col_array|
+   col_array.each do |name|
+     if !p_hash[name]
+         p_hash[name] = {}
+     end
+     if !p_hash[name][:color]
+         p_hash[name][:color] = []
+     end
+       p_hash[name][:color] << color.to_s
+   end
+ end
+
+ data[:gender].each do |gender, gen_array|
+   gen_array.each do |name|
+     if !p_hash[name]
+         p_hash[name] = {}
+     end
+     if !p_hash[name][:gender]
+        p_hash[name][:gender] = []
+     end
+       p_hash[name][:gender] << gender.to_s
+   end
+ end
+
+  data[:lives].each do |loc, loc_array|
+   loc_array.each do |name|
+     if !p_hash[name]
+         p_hash[name] = {}
+     end
+     if !p_hash[name][:lives]
+         p_hash[name][:lives] = []
+     end
+       p_hash[name][:lives] << loc.to_s
+   end
+ end
+   p_hash
 end
