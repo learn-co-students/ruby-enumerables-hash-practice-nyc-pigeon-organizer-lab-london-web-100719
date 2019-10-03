2f75bc0cc34db16fd907900d70c0c70ab1a15cd2
harmonious-monitor-8469 <a.shomalistos@gmail.com>
2019-10-03 13:16:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+    new = {}
+    data.map do |key, value|
+      value.map do |new_v, names|
+        names.map do |name|
+          
+          if !new[name]
+            new[name] = {}
+          end
+  
+          if !new[name][key]
+            new[name][key] = []
+          end
+  
+          new[name][key] << new_v.to_s
+  
+        end
+      end
+    end
+    new
+  end
